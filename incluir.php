2da5a976095ed1b963e44011cbd734622e69a200
Pablo Mendez <mevesa1901@gmail.com>
2017-04-08 20:28:14 -0300

    prueba en php

---
+++ incluir.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Funciones</title>
+</head>
+<body>
+	
+	<?php
+		echo"---------------------------------------------<br/>";
+		echo "Soy incluir.php<br>";
+        echo"Bueno compañero comencemos con esta tarea, comprendo mejor el gitkraken";
+	?>
+</body>
+</html>
