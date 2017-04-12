8a87a57505994e81ae3ac42a8a8b4454351562ad
Pablo Mendez <mevesa1901@gmail.com>
2017-04-12 01:31:08 -0300

    complemento FAQ

---
+++ ayuda.php
@@ -0,0 +1,231 @@
+<?php
+  include("header.php");
+?>
+<div class="jumbotron text-center fondoayuda">
+
+<style>
+
+    /* insertando color de fondo */
+    .fondoayuda {
+      background:#8DAFE9;
+      opacity:0.5;
+    }
+    </style>
+  <h2 class="textoayuda">
+      <style>
+              .textoayuda{
+              
+                  text-shadow:5px 5px 5px #fff;
+                  font-weight:bold;
+              }
+      </style>
+  ¿ Te puedo ayudar ?
+  </h2>
+  <p><div class="container">
+        <form>
+            <div class="row">
+              <div class="col-sm-3"></div>
+              <div class="col-sm-6">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Ingresa frase o palabra">
+                      <div class="input-group-btn">
+                          <button class="btn btn-default" type="submit">
+                            <i class="glyphicon glyphicon-search"></i>
+                          </button>
+                      </div>
+                  </div>
+              </div> 
+              <div class="col-sm-3"></div> 
+            </div>
+        </form>
+  </div></p>
+</div>
+<!--yyyyyy-->
+
+<!--ddddd-->
+<div class="container">
+  <h2>Preguntas frecuentes (FAQ)</h2>
+  <p><strong>Nota:</strong> Las <strong>respuestas</strong> de las siguientes preguntas lograrás visualizarlas al hacer <strong>Clik </strong>en las cajas siguientes.</p>
+  <div class="panel-group" id="accordion">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse1"><strong>¿Pregunta Item #01?</strong></a>
+        </h4>
+      </div>
+      <div id="collapse1" class="panel-collapse collapse in">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <hr>
+        <strong>¿Estas conforme con la respuesta?</strong>
+        <button type="button" class="btn btn-success">Sí</button>
+        <button type="button" class="btn btn-danger">No</button>
+        <hr>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse2"><strong>¿Pregunta Item #02?</strong></a>
+        </h4>
+      </div>
+      <div id="collapse2" class="panel-collapse collapse">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <hr>
+        <strong>¿Estas conforme con la respuesta?</strong>
+        <button type="button" class="btn btn-success">Sí</button>
+        <button type="button" class="btn btn-danger">No</button>
+        <hr>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse3"><strong>¿Pregunta Item #03?</strong></a>
+        </h4>
+      </div>
+      <div id="collapse3" class="panel-collapse collapse">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <hr>
+        <strong>¿Estas conforme con la respuesta?</strong>
+        <button type="button" class="btn btn-success">Sí</button>
+        <button type="button" class="btn btn-danger">No</button>
+        <hr>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse4"><strong>¿Pregunta Item #04?</strong></a>
+        </h4>
+      </div>
+      <div id="collapse4" class="panel-collapse collapse in">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <hr>
+        <strong>¿Estas conforme con la respuesta?</strong>
+        <button type="button" class="btn btn-success">Sí</button>
+        <button type="button" class="btn btn-danger">No</button>
+        <hr>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse5"><strong>¿Pregunta Item #05?</strong></a>
+        </h4>
+      </div>
+      <div id="collapse5" class="panel-collapse collapse in">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <hr>
+        <strong>¿Estas conforme con la respuesta?</strong>
+        <button type="button" class="btn btn-success">Sí</button>
+        <button type="button" class="btn btn-danger">No</button>
+        <hr>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse6"><strong>¿Pregunta Item #06?</strong></a>
+        </h4>
+      </div>
+      <div id="collapse6" class="panel-collapse collapse in">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <hr>
+        <strong>¿Estas conforme con la respuesta?</strong>
+        <button type="button" class="btn btn-success">Sí</button>
+        <button type="button" class="btn btn-danger">No</button>
+        <hr>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse7"><strong>¿Pregunta Item #07?</strong></a>
+        </h4>
+      </div>
+      <div id="collapse7" class="panel-collapse collapse in">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <hr>
+        <strong>¿Estas conforme con la respuesta?</strong>
+        <button type="button" class="btn btn-success">Sí</button>
+        <button type="button" class="btn btn-danger">No</button>
+        <hr>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse8"><strong>¿Pregunta Item #08?</strong></a>
+        </h4>
+      </div>
+      <div id="collapse8" class="panel-collapse collapse in">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <hr>
+        <strong>¿Estas conforme con la respuesta?</strong>
+        <button type="button" class="btn btn-success">Sí</button>
+        <button type="button" class="btn btn-danger">No</button>
+        <hr>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse9"><strong>¿Pregunta Item #09?</strong></a>
+        </h4>
+      </div>
+      <div id="collapse9" class="panel-collapse collapse in">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <hr>
+        <strong>¿Estas conforme con la respuesta?</strong>
+        <button type="button" class="btn btn-success">Sí</button>
+        <button type="button" class="btn btn-danger">No</button>
+        <hr>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse10"><strong>¿Pregunta Item #10?</strong></a>
+        </h4>
+      </div>
+      <div id="collapse10" class="panel-collapse collapse in">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <hr>
+        <strong>¿Estas conforme con la respuesta?</strong>
+        <button type="button" class="btn btn-success">Sí</button>
+        <button type="button" class="btn btn-danger">No</button>
+        <hr>
+        </div>
+      </div>
+    </div>
+  </div> 
+</div>
