df286ec58ebabb3721dc147bd995d9827da8c837
Sebastian Perez <sebastian.perez@alliansys.com.ar>
2017-04-15 13:04:40 -0300

    Header y footer con logo

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
 
   <body>
     <footer class="container-fluid text-center">
-      <p>Copiryght © 2017 Empresa | <a href="#">Términos y Condiciones</a></p>
+      <div class="row">
+        <div class="col-sm-9">
+          <p>Copyright © 2017 Service Advisor S.A. | <a href="#">Términos y Condiciones</a></p>
+        </div>
+        <div class="col-sm-3">
+          <img src="images/logoadvisor02.png" class="img-responsive" style="height:22px" alt="Image">
+        </div>
+
+      </div>
     </footer>
   </body>
 </html>
